fdf2fc791c1b06b9c7da3c0c50a9711d2df2b955
Felandric <abuguchie@yahoo.com>
2018-10-25 20:00:39 -0400

    contains and contains generator

---
+++ containsgenerator.py
@@ -0,0 +1,30 @@
+import csv
+import random
+
+#contains 
+
+with open("contains.csv", "w", newline='') as contains:
+	with open("sells.csv", newline='') as sells:
+		with open("issued.csv", newline='') as issued:
+			issuedReader = csv.reader(issued, delimiter=';', quotechar='"')
+			sellsReader = csv.reader(sells, delimiter=';', quotechar='"')
+			containsWriter = csv.writer(contains, delimiter=';')
+			
+			issuedList = list(issuedReader)[1:]
+			sellsList = list(sellsReader)[1:]
+						
+			containsWriter.writerow(["transactionid", "item", "quantity"])
+
+			minNumItems = 1
+			maxNumItems = 5
+			
+			minQuantity = 1
+			maxQuantity = 3
+			
+			for transaction in issuedList: 
+				potentialItems = [ sells for sells in sellsList if transaction[2] == sells[0] ]
+				numItemsBought = random.randint(minNumItems, maxNumItems)
+				itemsBought = random.sample(potentialItems, numItemsBought)
+				for item in itemsBought:
+					quantity = random.randint(minQuantity, maxQuantity)
+					containsWriter.writerow([transaction[0], item[1], quantity])
